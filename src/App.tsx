acacff6128d995795b7c141e482a301a2c277278
Lucas <lucas.pohl@hotmail.com>
2024-04-17 09:55:46 +0200

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import "./App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Counter from "./components/Counter/Counter";
-import Debounce from "./components/Debounce/Debounce";
 import Fetch from "./components/Fetch/Fetch";
+import Debounce from "./components/Debounce/Debounce";
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Custom Hooks</h1>
-      <Debounce />
-      <Counter />
-      <Fetch />
-    </>
+    <BrowserRouter>
+      <nav>
+        <Link to="/fetch">Fetch</Link>
+        <Link to="/counter">Counter</Link>
+        <Link to="/debounce">Debounce</Link>
+      </nav>
+      <Routes>
+        <Route path="/fetch" element={<Fetch />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/debounce" element={<Debounce />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
